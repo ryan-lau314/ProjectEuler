1f110a39cb9f300681d956c3cbc0876b9b8505eb
Ryan Lau <ryanlau394@gmail.com>
2020-12-28 14:39:43 +0000

    Optimize code for Problem 031

--- src/031.py
+++ src/031.py
@@ -5,18 +5,12 @@ if __name__ == '__main__':
     DP = [[0 for _ in range(VALUE + 1)] for _ in range(len(COINS))]
     for i in range(len(COINS)):
         coin_value = COINS[i]
-        for value in range(coin_value, VALUE + 1):
-            if value == 0:
-                DP[i][value] = 1
-            elif value < coin_value:
-                if i > 0:
-                    DP[i][value] = DP[i-1][value]
-                else:
-                    DP[i][value] = 0
+        for value in range(VALUE + 1):
+            if i == 0:
+                DP[i][value] = 1 if value % coin_value == 0 else 0
             else:
-                if i > 0:
-                    DP[i][value] = DP[i-1][value] + DP[i][value - coin_value]
-                else:
-                    DP[i][value] = DP[i][value - coin_value]
+                DP[i][value] = DP[i-1][value]
+                if coin_value <= value:
+                    DP[i][value] += DP[i][value - coin_value]
     
-    print(f'There are {DP[len(COINS) - 1][VALUE]} way(s) to make {VALUE}p')
+print(f'There are {DP[len(COINS) - 1][VALUE]} way(s) to make {VALUE}p')
