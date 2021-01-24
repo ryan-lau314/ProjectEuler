311ed86771917312731b22a6eb85172506a7197f
Ryan Lau <ryanlau394@gmail.com>
2021-01-24 14:08:43 +0000

    Publish Solution 039

---
+++ src/039.py
@@ -0,0 +1,17 @@
+if __name__ == "__main__":
+    # Maps permieters to valid triangle solutions
+    D = {}
+
+    for a in range(1, 1000 // 3):
+        for b in range(a, (1000 - a) // 2):
+            for c in range(b, 1000 - a - b):
+                if a * a + b * b == c * c:
+                    p = a + b + c
+                    if p in D:
+                        D[p].append((a, b, c))
+                    else:
+                        D[p] = [(a, b, c)]
+
+    max_record = max(D.items(), key=(lambda item: len(item[1])))
+    print(max_record)
+    print(len(max_record[1]))
