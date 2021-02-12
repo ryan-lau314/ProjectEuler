ed312c22d2e53cc0e3c0a2ca7016b8b6c01a4bdd
Ryan Lau <ryanlau394@gmail.com>
2021-02-12 20:36:29 +0000

    Publish Solution 045

---
+++ src/045.py
@@ -0,0 +1,36 @@
+from itertools import count
+
+
+def triangle():
+    for i in count(1):
+        yield i * (i + 1) // 2
+
+
+def pentagon():
+    for i in count(1):
+        yield i * (3 * i - 1) // 2
+
+
+def hexagon():
+    for i in count(1):
+        yield i * (2 * i - 1)
+
+
+def merge(first_gen, second_gen):
+    first = next(first_gen)
+    second = next(second_gen)
+    while True:
+        if first < second:
+            first = next(first_gen)
+        elif first > second:
+            second = next(second_gen)
+        else:
+            yield first
+            first = next(first_gen)
+            second = next(second_gen)
+
+if __name__ == '__main__':
+    common_generator = merge(merge(triangle(), pentagon()), hexagon())
+    next(common_generator)
+    next(common_generator)
+    print(next(common_generator))
