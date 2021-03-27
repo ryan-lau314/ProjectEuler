57daebb889c0ada0c03df6bb8852dff7bb9882ab
Ryan Lau <ryanlau394@gmail.com>
2021-03-27 10:41:01 +0000

    Refactor Solution 048

--- src/048.py
+++ src/048.py
@@ -3,5 +3,5 @@ MODULUS = 10_000_000_000
 if __name__ == "__main__":
     sum = 0
     for i in range(1, 1001):
-        sum = (sum + pow(i, i, 10_000_000_000)) % MODULUS
+        sum = (sum + pow(i, i, MODULUS)) % MODULUS
     print(sum)
