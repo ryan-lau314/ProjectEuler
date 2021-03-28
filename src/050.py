7413a3843ed82f78fb83c4c8c427c6a94db20129
Ryan Lau <ryanlau394@gmail.com>
2021-03-28 17:13:55 +0100

    Refactor solution 050

--- src/050.py
+++ src/050.py
@@ -3,21 +3,35 @@ from itertools import accumulate
 
 MAX_VALUE = 1_000_000
 
-if __name__ == "__main__":
-    prime_list = [i for i in sieve.primerange(1, MAX_VALUE)]
-    prime_acc = [0] + list(accumulate(prime_list))
-    prime_set = set(prime_list)
+prime_list = [i for i in sieve.primerange(1, MAX_VALUE)]
+prime_acc = [0] + list(accumulate(prime_list))
+prime_set = set(prime_list)
+
+
+def p050():
     n = len(prime_list)
+    largest_prime = prime_list[n - 1]
 
-    max_chain_size = next(
-        index for index, value in enumerate(prime_acc) if value > MAX_VALUE
-    )
+    for l in range(n, 0, -1):
+        for i in range(0, n - l + 1):
+            prime_sum = sublist_sum(i, i + l - 1)
 
-    for chain_size in range(max_chain_size, 0, -1):
-        for i in range(0, n - chain_size):
-            prime_sum = prime_acc[i + chain_size] - prime_acc[i]
+            # Terminate early when the prime sum is larger than the largest prime.
+            # Prime sums at later i's are larger, therefore not helpful.
+            if prime_sum > largest_prime:
+                break
+
+            # Use set instead of list for faster searching
             if prime_sum in prime_set:
-                print(
-                    f"{prime_sum} = {prime_list[i + 1]} + ... + {i + chain_size} ({chain_size} primes)"
-                )
-                exit(0)
+                return (prime_sum, i, l)
+
+
+def sublist_sum(i, j):
+    if i == 0:
+        return prime_acc[j]
+    return prime_acc[j] - prime_acc[i - 1]
+
+
+if __name__ == "__main__":
+    s, i, l = p050()
+    print(f"{s} = {prime_list[i]} + ... + {prime_list[i + l - 1]} ({l} primes)")
